bb6c859a3c183dd1951d8841562754643dec584a
Muhammad-mansur <muhammadmansur737@gmail.com>
2024-04-23 10:15:48 -0700

    1-fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file
 
 file { '/tmp/school':
-    ensure => present,
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love puppet'
 }
